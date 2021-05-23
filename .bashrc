f7a050e93d37ba5f3be19e96ccb14e44c2277375
Keith Plant <kplantjr@gmail.com>
2021-05-23 01:30:46 -0400

    add simple installation script

--- .bashrc
+++ .bashrc
@@ -1,3 +1,8 @@
+[[ $- != *i* ]] && return
+
+#export PS1='[\u@\h \W]\$ '
+#export PS1='\u@\h:\W\$ '
+export PS1="\[\033[38;5;12m\]\u\[$(tput sgr0)\]@\[\033[38;5;10m\]\h\[$(tput sgr0)\]:\[\033[38;5;11m\]\w\[$(tput sgr0)\]\\$ "
 export HISTSIZE=1000000
 
 alias diff='diff --color=auto'
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+dotfiles=(
+	.alacritty.yml
+	.bash_profile
+	.bashrc
+	.tmux.conf
+	.vimrc
+	.ycm_extra_conf.py
+)
+
+echo "This script will place **hard links** in your home directory to "
+echo "the following files:"
+echo
+
+for i in "${dotfiles[@]}"; do echo $i; done
+
+echo
+echo "**IF THESE FILES ARE ALREADY PRESENT THEY WILL BE REPLACED**"
+read -p "Are you sure you want to continue? [y/n] " -n 1 -r
+echo
+
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+	[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+for i in "${dotfiles[@]}"; do
+	rm -f ~/$i
+	ln -v $i ~
+done
